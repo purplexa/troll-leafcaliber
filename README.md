20f87e8510ef1ad2cd09abe28ea2aa50a59b8e42
Lexa Whitehurst <me@purplexa.com>
2025-12-08 14:36:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A Tool to Mildly Annoying LeafCaliber
---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./App.css";
+import {
+  Box,
+  ChakraProvider,
+  defaultSystem,
+  Field,
+  Heading,
+  Stack,
+  Textarea,
+} from "@chakra-ui/react";
+
+function Page() {
+  const [text, setText] = useState("");
+  const onChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setText(e.currentTarget.value);
+  };
+  const convertedText = text.replaceAll(/\s*\w+/g, (match) => `||${match}||`);
+  return (
+    <Box>
+      <Heading>LeafCaliber Spoiler Trolling Tool</Heading>
+      <Stack gap="4">
+        <Textarea
+          placeholder="Message to convert..."
+          autoresize
+          onChange={onChange}
+        />
+        <Field.Root
+          onClick={() => {
+            navigator.clipboard.writeText(convertedText);
+          }}
+        >
+          <Textarea disabled autoresize value={convertedText} />
+          <Field.HelperText style={{ cursor: "pointer" }}>
+            Click to copy.
+          </Field.HelperText>
+        </Field.Root>
+      </Stack>
+    </Box>
+  );
+}
+
+function App() {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <Page />
+    </ChakraProvider>
+  );
+}
+
+export default App;
