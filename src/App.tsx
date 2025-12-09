079a4500c859ac6dd331cdead62b76803248b8fe
Lexa Whitehurst <me@purplexa.com>
2025-12-08 19:01:51 -0800

    Add multiple spoilerification options

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import {
   Box,
@@ -6,16 +6,50 @@ import {
   defaultSystem,
   Field,
   Heading,
+  HStack,
+  RadioGroup,
   Stack,
   Textarea,
 } from "@chakra-ui/react";
 
+type TrollType = "word" | "random" | "letter";
+
+const convertText = (text: string, trollType: TrollType) => {
+  let ret = "||";
+  switch (trollType) {
+    case "word":
+      return text.replaceAll(/\s*\S+/g, (match) => `||${match}||`);
+    case "letter":
+      return text.replaceAll(/./g, (match) => `||${match}||`);
+    case "random":
+      for (const letter of text.substring(0, text.length - 1)) {
+        const check = Math.floor(Math.random() * 3);
+        if (check === 0) {
+          ret = `${ret}${letter}||||`;
+        } else {
+          ret = `${ret}${letter}`;
+        }
+      }
+      ret = `${ret}${text[text.length - 1]}||`;
+  }
+  return ret;
+};
+
 function Page() {
   const [text, setText] = useState("");
-  const onChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-    setText(e.currentTarget.value);
+  const [trollText, setTrollText] = useState("");
+  const [trollType, setTrollType] = useState<TrollType>("word");
+  const onTextChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    const val = e.currentTarget.value;
+    setText(val);
+    setTrollText(convertText(val, trollType));
   };
-  const convertedText = text.replaceAll(/\s*\S+/g, (match) => `||${match}||`);
+  const onTypeChange = (e: RadioGroup.ValueChangeDetails) => {
+    const val: TrollType = (e.value as TrollType) || "word";
+    setTrollType(val);
+    setTrollText(convertText(text, val));
+  };
+
   return (
     <Box>
       <Heading>LeafCaliber Spoiler Trolling Tool</Heading>
@@ -23,14 +57,33 @@ function Page() {
         <Textarea
           placeholder="Message to convert..."
           autoresize
-          onChange={onChange}
+          onChange={onTextChange}
         />
+        <RadioGroup.Root value={trollType} onValueChange={onTypeChange}>
+          <HStack>
+            <RadioGroup.Item value="word">
+              <RadioGroup.ItemHiddenInput />
+              <RadioGroup.ItemIndicator />
+              <RadioGroup.ItemText>By Word</RadioGroup.ItemText>
+            </RadioGroup.Item>
+            <RadioGroup.Item value="letter">
+              <RadioGroup.ItemHiddenInput />
+              <RadioGroup.ItemIndicator />
+              <RadioGroup.ItemText>By Letter</RadioGroup.ItemText>
+            </RadioGroup.Item>
+            <RadioGroup.Item value="random">
+              <RadioGroup.ItemHiddenInput />
+              <RadioGroup.ItemIndicator />
+              <RadioGroup.ItemText>Randomly</RadioGroup.ItemText>
+            </RadioGroup.Item>
+          </HStack>
+        </RadioGroup.Root>
         <Field.Root
           onClick={() => {
-            navigator.clipboard.writeText(convertedText);
+            navigator.clipboard.writeText(trollText);
           }}
         >
-          <Textarea disabled autoresize value={convertedText} />
+          <Textarea disabled autoresize value={trollText} />
           <Field.HelperText style={{ cursor: "pointer" }}>
             Click to copy.
           </Field.HelperText>
